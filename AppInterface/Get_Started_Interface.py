7c29803fa7f93b5c7e2444eb185f81cd941059a3
Nguyễn Hoàng Đăng Khoa <159506928+UITshirodoragon@users.noreply.github.com>
2024-08-07 12:33:08 +0700

    Revert "ocj ocj ocj"

--- AppInterface/Get_Started_Interface.py
+++ AppInterface/Get_Started_Interface.py
@@ -1,58 +1,20 @@
 import customtkinter as ctk
 import Image_Capture_Interface as ICI
-from customtkinter import CTkFont
-from PIL import ImageFont, ImageDraw, Image, ImageTk
 
 class Get_Started_Interface(ctk.CTkFrame):
-    
-    def __init__(self, root):
+    def __init__(self, parent):
+        # inherit from CTkFrame
+        super().__init__(master = parent)
+        self.state = True # state of object: True = activate, False = deactivate
+        self.parent = parent
 
-        self.color1 = '#BD8D5F'
-        self.color2 = '#24685B'
-        self.color3 = '#9ECEEA' 
-        self.color4 = '#F8FCFD'
-        self.root = root
-        
-        header_font = CTkFont(family=ImageFont.truetype("DataStorage/Fonts/UTM Thanh Nhac TL.ttf", size=50).getname()[0], size=50)
-        desciption_font = CTkFont(family=ImageFont.truetype("DataStorage/Fonts/UTM Thanh Nhac TL.ttf", size=50).getname()[0], size=30)
+        # 'Press to start' label
+        self.label = ctk.CTkLabel(self,
+                                   text = 'Press to start',
+                                   font = ('Arial', 20))
 
-        frame = ctk.CTkFrame(root, fg_color=self.color2, corner_radius=20)
-        frame.pack(pady=20, padx=20, fill="both")
-
-        header_label = ctk.CTkLabel(master=frame, 
-                                    text="PHOTO BOOTH", 
-                                    font=header_font,
-                                    text_color=self.color4)
-        header_label.grid(column = 0, row = 0, padx = 40, pady = (70, 0))
-
-        description_label = ctk.CTkLabel(master=frame, 
-                                    text="Sản phẩm trưng bày",  
-                                    font=desciption_font,
-                                    text_color=self.color4)
-        description_label.grid(column = 0, row = 1, padx = 40, pady = (0, 130), sticky = ctk.W)
-
-        smt = ctk.CTkLabel(master=frame,
-                            text="cái gì đó",
-                            width=260,
-                            height=200,  
-                            font=desciption_font,
-                            fg_color=self.color4,
-                            text_color=self.color3,
-                            corner_radius=10)
-        smt.grid(column = 1, row = 0, padx = (150, 0), pady = (80, 0), rowspan = 2)
-
-        button = ctk.CTkButton(master=root,
-                               width=1024,
-                               height=300,
-                               text="tap to start!", 
-                               text_color=self.color3,
-                               font=desciption_font,
-                               bg_color=self.color4,
-                               fg_color=self.color4,
-                               hover_color=self.color4,
-                               command=lambda root = root: change_page(root))
-        button. pack()
-
-        def change_page(self):
-            for child in root.winfo_children():
-                child.destroy()
+        # Layout
+        # Put the label in the center of the frame
+        self.label.place(relx = 0.5,
+                          rely = 0.5,
+                         anchor = 'center')
